a1832481c485430d4a8e3a3400c994a5300d8a0a
AmirHTaheri <amir.houshang.taheri@gmail.com>
2017-09-18 13:26:40 +0200

    Message removed!

--- NewTestApplication/NewTestApplication/Program.cs
+++ NewTestApplication/NewTestApplication/Program.cs
@@ -11,7 +11,6 @@ namespace NewTestApplication
         static void Main(string[] args)
         {
             Console.WriteLine("This is a test!");
-            Console.WriteLine("Another message!");
         }
     }
 }
